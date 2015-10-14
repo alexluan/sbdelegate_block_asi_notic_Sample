18ad72b534707670d3385fd1d4b309d874e4ca72
alexluan <326083325@qq.com>
2015-10-15 03:07:12 +0800

    Initial Commit

---
+++ sbsb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sbsb
+//
+//  Created by 栾有数 on 15/10/15.
+//  Copyright © 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sbsb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sbsb
+//
+//  Created by 栾有数 on 15/10/15.
+//  Copyright © 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
